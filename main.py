b85091018bcf96bca614bd720ea7da4680402026
Ortiane <dchang2@andrew.cmu.edu>
2020-04-20 21:23:33 -0400

    Fixed everything, even my dog

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
-import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
-from model import *
 import argparse
+import os
+
+import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
+from sklearn.metrics import auc, roc_curve
+from sklearn.model_selection import train_test_split
+
+from model import *
 from preprocess import *
+
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
+
 # tf.config.experimental.set_visible_devices([], 'GPU')
+
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -15,6 +23,7 @@ def parse_args():
     args = parser.parse_args()
     return args
 
+
 def main():
     # Get arguments
     args = parse_args()
@@ -22,48 +31,42 @@ def main():
     OUTPUT_SIZE = args.output_size
     MAX_SEQ_LEN = args.max_seq_len
     # Read in data
-    x_train, y_train = process_data(DATA_FILE,MAX_SEQ_LEN)
-    print(f"Number of examples is {len(x_train)} ")
+    X, y = process_data(DATA_FILE, MAX_SEQ_LEN)
+    print(f"Number of examples is {len(X)} ")
     # Build and run model
-    model = Model(
-        output_size=OUTPUT_SIZE, 
-    )
-    model.compile(
-        loss='binary_crossentropy', 
-        optimizer='adagrad', 
-        metrics=['accuracy']
-    )
-    model.build(input_shape=(MAX_SEQ_LEN,3))
+    model = Model(output_size=OUTPUT_SIZE,)
+    model.compile(loss="binary_crossentropy", optimizer="adagrad", metrics=["accuracy"])
+    model.build(input_shape=(MAX_SEQ_LEN, 3))
     model.summary()
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=18)
+
     model.fit(
-        x=x_train,
+        x=X_train,
         y=y_train,
-        batch_size=16,
-        epochs=35,
+        batch_size=32,
+        epochs=25,
         verbose=1,
-        validation_split=0.2,
+        validation_data=(X_test, y_test),
         shuffle=True,
-        validation_freq=1
+        validation_freq=1,
     )
-    # predict on the training set itself
-    result = model.predict(
-        x=x_train[::100],
-        verbose=0
-    )
-    size = len(y_train)
-    accu_count = 0
-    for (predict, true) in zip(result, y_train[::100]):
-        final = 0
-        if predict >= 0.5:
-            final = 1
-        if final == true:
-            accu_count += 1
-        print(f"Predict label {final}, true label {true}")
-    print(f"Accuracy is {accu_count / size}, benchmark is {1 - np.sum(y_train) / size}")
+
+    # https://www.dlology.com/blog/simple-guide-on-how-to-generate-roc-plot-for-keras-classifier/
+    y_pred = model.predict(X_test).ravel()
+    fpr, tpr, thresholds = roc_curve(y_test, y_pred)
+    auc_value = auc(fpr, tpr)
+    
+    plt.figure(1)
+    plt.plot([0, 1], [0, 1], 'k--')
+    plt.plot(fpr, tpr, label='LSTM (area = {:.3f})'.format(auc_value))
+    plt.xlabel('False positive rate')
+    plt.ylabel('True positive rate')
+    plt.title('ROC curve')
+    plt.legend(loc='best')
+    plt.savefig('ROC_curve.png')
 
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
-
-
-
--- preprocess.py
+++ preprocess.py
@@ -3,45 +3,40 @@ import numpy as np
 import tensorflow as tf
 from web3.auto import w3
 
+
 def process_data(file_path, max_seq_len):
     x_train = []
     y_train = []
     with open(file_path) as f:
-        csv_reader = csv.reader(f, delimiter=',')
+        csv_reader = csv.reader(f, delimiter=",")
         gambling = 0
         non_gambling = 0
         for i, row in enumerate(csv_reader):
             x = row[2:]
             y = int(row[0])
-            #print(len(x))
+            # print(len(x))
             for ele in range(len(x)):
-                x[ele] = float(w3.fromWei(int(x[ele]),'gwei'))
-            x = np.array(x).reshape(-1,3)
-            #remove gambling contracts that are all zeros (you can't be gambling with no money)
+                x[ele] = float(w3.fromWei(int(x[ele]), "gwei"))
+            x = np.array(x).reshape(-1, 3)
+            # remove gambling contracts that are all zeros (you can't be gambling with no money)
             if not np.any(x) and y == 0:
-              continue
+                continue
             x_train.append(x)
             y_train.append(y)
-            if y==1:
-              gambling+=1
+            if y == 1:
+                gambling += 1
             else:
-              non_gambling+=1
-
-
-        print(gambling,non_gambling)
-
-
+                non_gambling += 1
+        print(gambling, non_gambling)
 
     x_train = tf.keras.preprocessing.sequence.pad_sequences(
-                    x_train,
-                    dtype='float64',
-                    maxlen=max_seq_len,
-                    padding='post',
-                    value=-1
-                )
+        x_train, dtype="float64", maxlen=max_seq_len, padding="post", value=-1
+    )
 
     y_train = np.array(y_train)
     return x_train, y_train
 
-if __name__ == '__main__':
-    process_data('data.csv')
+
+if __name__ == "__main__":
+    process_data("data.csv")
+
